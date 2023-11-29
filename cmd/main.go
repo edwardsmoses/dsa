303eb5d1149a49884bbb8824dd10437dcaddd7f1
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-30 00:54:27 +0100

    update naming

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,14 @@ import  (
 "fmt"
 )
 
-func BigO() {
+func ConstantBigO() {
 arr := [4] int {1,2,3,4}
 fmt.Println(arr[1]);
 }
 
 func main() {
 	fmt.Println("Welcome - writing in terminal")
-BigO();
+ConstantBigO();
 }
 
 
