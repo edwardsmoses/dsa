ae1940d64a106cb1ed3b2d9797fea01fd77ae32f
Edwards Moses <edwardsmoses3@gmail.com>
2023-11-30 01:02:52 +0100

    more understanding about space in terms of memory spaces

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,21 @@
-package main 
+package main
 
-import  (
-"fmt"
+import (
+	"fmt"
 )
 
 func ConstantBigO() {
-arr := [4] int {1,2,3,4}
-fmt.Println(arr[1]);
+	arr := [4]int{1, 2, 3, 4}
+	fmt.Println(arr[1])
 
-arr[0] = arr[0] + 10;
-fmt.Println(arr[0]);
+	num := arr[len(arr)-1]
+	fmt.Println("num", num)
+
+	arr[0] = arr[0] + 10
+	fmt.Println(arr[0])
 }
 
 func main() {
 	fmt.Println("Welcome - writing in terminal")
-ConstantBigO();
+	ConstantBigO()
 }
-
-
